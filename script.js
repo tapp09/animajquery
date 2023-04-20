48cc1275451f1c7e47b37ade7174b71f4853a391
tapp09 <caio_r_rosa@estudante.sesisenai.org.br>
2023-04-20 11:05:21 -0300

    opa

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+setInterval(movebolinha, 2000)
+
+function movebolinha() {
+    $("#bolinha").animate({left:'590'}, 1000)
+    $("#bolinha").animate({left:'590', top: '590'}, 1000)
+    $("#bolinha").animate({left:'-10', top: '590'}, 1000)
+    $("#bolinha").animate({left:'-10', top: '-10'}, 1000)
+}
 function subir() {
     $("#menu").slideUp(2000)
+    $("#menu").animate({right:'50'}, 1000)
 }
 function descer() {
     $("#menu").slideDown(2000)
+    $("#menu").animate({right:'100'})
+
 }
